f7f34d49b3218fc06ff88f17c7e15b8f31a77ebd
GayaniPKarunaratne <gayanikarunaratne222@gmail.com>
2019-09-03 15:06:18 +0530

    Save and Show methods

--- app/src/main/java/com/example/student/newone/MainActivity.java
+++ app/src/main/java/com/example/student/newone/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.student.newone;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,13 +9,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText txt_id,txt_name,txt_address,txt_contact;
-    Button btn_save,btn_show,btn_update,btn_delete;
+    EditText txt_id, txt_name, txt_address, txt_contact;
+    Button btn_save, btn_show, btn_update, btn_delete;
     Student student;
     DatabaseReference dbRsf;
 
@@ -38,22 +42,53 @@ public class MainActivity extends AppCompatActivity {
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dbRsf =FirebaseDatabase.getInstance().getReference().child("Student");
+                dbRsf = FirebaseDatabase.getInstance().getReference().child("Student");
 
                 student.setId(txt_id.getText().toString().trim());
                 student.setAddress(txt_address.getText().toString().trim());
                 student.setName(txt_name.getText().toString().trim());
                 student.setContactNum(Integer.parseInt(txt_contact.getText().toString().trim()));
 
-                dbRsf.push().setValue(student);
-                Toast.makeText(getApplicationContext(),"Adding Succes",Toast.LENGTH_LONG).show();
+                // dbRsf.push().setValue(student);
+                dbRsf.child("st1").setValue(student);
+                Toast.makeText(getApplicationContext(), "Adding Succes", Toast.LENGTH_LONG).show();
                 clearData();
 
             }
         });
 
 
+        btn_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference readRef = FirebaseDatabase.getInstance().getReference().child("Student").child("st1");
+                readRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChildren()){
+                            txt_id.setText(dataSnapshot.child("id").getValue().toString());
+                            txt_name.setText(dataSnapshot.child("name").getValue().toString());
+                            txt_address.setText(dataSnapshot.child("address").getValue().toString());
+                            txt_contact.setText(dataSnapshot.child("contactNum").getValue().toString());
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),"No Source to display",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+
+
     }
+
+
 
     public void clearData(){
         txt_id.setText("");
